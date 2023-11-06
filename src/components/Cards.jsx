d0fc9ff63cd8e7ffd71157c46b1dc6ed5f3fc6ef
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-11-06 11:37:17 +0300

    commit -2

---
+++ src/components/Cards.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Cards() {
+    return (
+    <div className='cards'>
+        <div className='buttons'>
+            <button>Why React?</button>
+            <button>Core Features</button>
+            <button>Related Resources</button>
+        </div>
+
+        <div className='body-content'>
+            <li> React is extremely popular</li>
+            <li> It makes building complex, interactive Uls a breeze</li>
+            <li> It's powerful & flexible</li>
+            <li> It has a very active and versatile ecosystem</li>
+        </div>
+
+
+
+    </div>
+    )
+}
+
+export default Cards
